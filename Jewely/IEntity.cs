542d40eb89a879d6f219f19ff7fb95acc364f1be
Hunter Krieger <hckrieger77@outlook.com>
2026-06-21 14:27:05 -0400

    add IEntity

---
+++ Jewely/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jewely
+{
+	public interface IEntity
+	{
+		public int ID { get; }
+	}
+}
---
+++ Jewely/RenderableDataBatcher.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Jewely
+{
+	public class RenderableDataBatcher
+	{
+
+		private RenderableDataStore data;
+		private ContentManager Content;
+		private int nth = 0;
+		public RenderableDataBatcher(Game game)
+		{
+			Content = game.Services.GetService<ContentManager>();
+		}
+
+		public void AddDataEntity(RenderableDataInstance dataInstance)
+		{
+			data.TextureKey[nth] = dataInstance.TextureKey;
+			data.Position[nth] = dataInstance.Position;
+			data.SourceRectangle[nth] = dataInstance.SourceRectangle;
+			data.Color[nth] = dataInstance.Color;
+			data.Rotation[nth] = dataInstance.Rotation;
+			data.Origin[nth] = dataInstance.Origin;
+			data.Scale[nth] = dataInstance.Scale;
+			data.SpriteEffects[nth] = dataInstance.SpriteEffects;
+			data.LayerDepth[nth] = dataInstance.LayerDepth;
+
+			nth++;
+		}
+
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			for (int i = 0; i < nth; i++) 
+			{
+				spriteBatch.Draw(
+					Content.Load<Texture2D>(data.TextureKey[i]),
+					data.Position[i],
+					data.SourceRectangle[i],
+					data.Color[i],
+					data.Rotation[i],
+					data.Origin[i],
+					data.Scale[i],
+					data.SpriteEffects[i],
+					data.LayerDepth[i]
+					);
+			}
+		}
+	
+	}
+
+	public struct RenderableDataStore
+	{
+		public string[] TextureKey;
+		public Vector2[] Position;
+		public Rectangle[] SourceRectangle;
+		public Color[] Color;
+		public int[] Rotation;
+		public Vector2[] Origin;
+		public Vector2[] Scale;
+		public SpriteEffects[] SpriteEffects;
+		public float[] LayerDepth;
+		
+	}
+
+	public struct RenderableDataInstance
+	{
+		public string TextureKey;
+		public Vector2 Position;
+		public Rectangle SourceRectangle;
+		public Color Color;
+		public int Rotation;
+		public Vector2 Origin;
+		public Vector2 Scale;
+		public SpriteEffects SpriteEffects;
+		public float LayerDepth;
+		
+	}
+
+
+}
